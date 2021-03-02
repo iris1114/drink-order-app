7bbb88e3ca4af378d50d67cc8edb7b05e2297a80
iris1114 <qweichew@gmail.com>
2021-03-02 14:23:19 +0800

    remove mark

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
 
-/**
- * hint:
- * 更新狀態時記得使用 immutable 的方式
- * ex. setDrinks([...drinks, newDrink])
- */
-
 const App = () => {
   const defaultDrinks = [
     {
       name: "烏龍綠",
       options: "半糖去冰",
-      buyer: "kk"
+      buyer: "iris"
     },
     {
       name: "珍奶",
       options: "無糖少冰",
-      buyer: "ken"
+      buyer: "nicole"
     }
   ];
   const [drinks, setDrinks] = useState(defaultDrinks);
@@ -41,7 +35,7 @@ const App = () => {
         <DrinkInputGroup onCreate={handleDrinkCreate} />
         {drinks.map((drink, index) => (
           <DrinkCard
-            key={v4()} //uuid or JSON.stringify(drink)
+            key={v4()} 
             index={index}
             buyer={drink.buyer}
             name={drink.name}
